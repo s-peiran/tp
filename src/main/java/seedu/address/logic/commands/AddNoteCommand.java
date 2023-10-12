0fbfc43776acbeff0987ba3b775b160b621d31d2
jacobcuison <e0969947@u.nus.edu>
2023-10-13 01:30:08 +0800

    Create AddNoteCommandParser

--- src/main/java/seedu/address/logic/commands/AddNoteCommand.java
+++ src/main/java/seedu/address/logic/commands/AddNoteCommand.java
@@ -1,2 +1,68 @@
-package seedu.address.logic.commands;public class AddNoteCommand {
+package seedu.address.logic.commands;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NOTE_CONTACT;
+import seedu.address.model.Model;
+
+/**
+ * Changes the note of an existing contact in the address book.
+ */
+public class AddNoteCommand extends Command {
+
+    public static final String COMMAND_WORD = "note";
+
+    public static final String MESSAGE_ARGUMENTS = "Index: %1$d, Remark: %2$s";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Edits the remark of the person identified "
+            + "by the index number used in the last person listing. "
+            + "Existing remark will be overwritten by the input.\n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + "r/ [REMARK]\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + "r/ Likes to swim.";
+
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET =
+            "Remark command not implemented yet";
+
+    private final Index index;
+    private final String remark;
+
+    /**
+     * @param index of the person in the filtered person list to edit the remark
+     * @param remark of the person to be updated to
+     */
+    public AddNoteCommand(Index index, String remark) {
+        requireAllNonNull(index, remark);
+
+        this.index = index;
+        this.remark = remark;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException(String.format(MESSAGE_ARGUMENTS, index.getOneBased(), remark));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof AddNoteCommand)) {
+            return false;
+        }
+
+        // state check
+        AddNoteCommand e = (AddNoteCommand) other;
+        return index.equals(e.index)
+                && remark.equals(e.remark);
+    }
+
 }
