28d677c1f856d754cff2b90eef912985c67d3221
jacobcuison <e0969947@u.nus.edu>
2023-10-13 10:53:44 +0800

    Fix style errors

--- src/main/java/seedu/address/logic/commands/AddNoteCommand.java
+++ src/main/java/seedu/address/logic/commands/AddNoteCommand.java
@@ -5,7 +5,6 @@ import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 import java.util.List;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
