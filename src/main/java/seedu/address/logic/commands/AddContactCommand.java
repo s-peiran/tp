7573d0d7d910d36d4ed4f9d9882464e27e12294a
jacobcuison <122289229+jacobcuison@users.noreply.github.com>
2023-11-02 15:24:12 +0800

    Update AddContactCommand message usage string
    Remove note prefix in message usage string.

--- src/main/java/seedu/address/logic/commands/AddContactCommand.java
+++ src/main/java/seedu/address/logic/commands/AddContactCommand.java
@@ -35,8 +35,7 @@ public class AddContactCommand extends Command {
             + PREFIX_EMAIL + " johnd@example.com "
             + PREFIX_ADDRESS + " 311, Clementi Ave 2, #02-25 "
             + PREFIX_TAG + " friends "
-            + PREFIX_TAG + " owesMoney "
-            + PREFIX_NOTE + " some note";
+            + PREFIX_TAG + " owesMoney";
 
     public static final String MESSAGE_SUCCESS = "New contact added: %1$s";
     public static final String MESSAGE_DUPLICATE_CONTACT = "This contact already exists in the address book";
