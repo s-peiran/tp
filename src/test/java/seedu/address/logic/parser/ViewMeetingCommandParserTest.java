21edec842b0573812bd56daf3ef8f71201319a51
chiayunrong <chiayunrong@outlook.com>
2023-11-07 16:30:51 +0800

    Fix view command feature flaw

--- src/test/java/seedu/address/logic/parser/ViewMeetingCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ViewMeetingCommandParserTest.java
@@ -1,12 +1,12 @@
 package seedu.address.logic.parser;
 
-import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
 import seedu.address.logic.commands.ViewMeetingCommand;
 
 public class ViewMeetingCommandParserTest {
@@ -15,17 +15,16 @@ public class ViewMeetingCommandParserTest {
 
     @Test
     public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                ViewMeetingCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "     ", Messages.MESSAGE_INVALID_MEETING_DISPLAYED_INDEX);
     }
 
     @Test
     public void parse_validArgs_returnsViewMeetingCommand() {
         // no leading and trailing whitespaces
         ViewMeetingCommand expectedViewMeetingCommand = new ViewMeetingCommand(Index.fromOneBased(1));
-        assertParseSuccess(parser, " " + CliSyntax.PREFIX_INDEX + "1", expectedViewMeetingCommand);
+        assertParseSuccess(parser, " 1", expectedViewMeetingCommand);
 
         // multiple whitespaces between keywords
-        assertParseSuccess(parser, " " + CliSyntax.PREFIX_INDEX + " \n 1 \n", expectedViewMeetingCommand);
+        assertParseSuccess(parser, " \n 1 \n", expectedViewMeetingCommand);
     }
 }
